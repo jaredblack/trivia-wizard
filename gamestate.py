2c5f764819af9bbdd6d0c77d34bf2e101871b0bc
jaredblack <jrdblck@gmail.com>
2023-03-06 22:08:00 -0700

    Factor game state out of server code

---
+++ gamestate.py
@@ -0,0 +1,34 @@
+
+from database import TriviaDatabase
+
+
+class Game:
+    def __init__(self, game_code, host_socket):
+        self.game_code = game_code
+        self.question_index = 1
+        self.host_socket = host_socket
+        self.players = []
+        self.db = TriviaDatabase()        
+    
+    def add_player(self, player_socket):
+        self.players.append(player_socket)
+
+    def increment_question_index(self):
+        self.question_index += 1
+        return self.question_index
+
+    def decrement_question_index(self):
+        if (self.question_index > 1):
+            self.question_index -= 1
+        return self.question_index
+    
+    def get_answers(self):
+        answers = self.db.get_answers(self.game_code, self.question_index)
+        if answers is None:
+            answers = []
+        return answers
+    
+    def add_answer(self, answer):
+        self.db.insert_answer(answer)
+    
+    
--- server.py
+++ server.py
@@ -8,12 +8,9 @@ import os
 import signal
 
 from database import Answer, TriviaDatabase
+from gamestate import Game
 
-HOST = {}
-PLAYERS = []
-GAME_CODE_LENGTH = 5
 
-# TODO: Game state (code, question index, players, host) should be abstracted away in some sort of Game class. Right now everything is bound to the instance of the server which means the game code doesn't really do anything
 class Server:
     def __init__(self) -> None:
         asyncio.run(self.main())
@@ -26,105 +23,96 @@ class Server:
         await websocket.send(json.dumps(event))
 
 
-    async def host(self, websocket):
-        async for message in websocket:
+    async def host(self, host_socket, game):
+        async for message in host_socket:
             event = json.loads(message)
             if event["type"] == "next":
-                await self.return_next_question(websocket)
+                game.increment_question_index()
+                await self.broadcast_new_question(game)
             elif event["type"] == "prev":
-                await self.return_prev_question(websocket)
+                game.decrement_question_index()
+                await self.broadcast_new_question(game)
             else:
-                await self.send_error(websocket, "Unknown host request type")
+                await self.send_error(host_socket, "Unknown host request type")
 
-    async def return_next_question(self, websocket):
-        self.question_index += 1
-        answers = self.db.get_answers(self.game_code, self.question_index)
-        if answers == None:
-            answers = []
+    async def broadcast_new_question(self, game):
+        await self.update_player_question_index(game)
+        await self.update_host_question(game)
+
+    async def update_host_question(self, game):
+        answers = game.get_answers()
         event = {
             "type": "newQuestion",
-            "questionIndex": self.question_index,
+            "questionIndex": game.question_index,
             "answers": answers
         }
-        await self.update_player_question_index()
-        await websocket.send(json.dumps(event))
-
-    async def return_prev_question(self, websocket):
-        if (self.question_index > 1):
-            self.question_index -= 1
-        answers = self.db.get_answers(self.game_code, self.question_index)
-        if answers == None:
-            answers = []
-        event = {
-            "type": "newQuestion",
-            "questionIndex": self.question_index,
-            "answers": answers
-        }
-        await self.update_player_question_index()
-        await websocket.send(json.dumps(event))
+        await game.host_socket.send(json.dumps(event))
     
-    async def update_player_question_index(self):
+    async def update_player_question_index(self, game):
         # TODO: include info about whether each team needs to provide an answer for this question or not.
         event = {
             "type": "updatePlayerQuestionIndex",
-            "questionIndex": self.question_index
+            "questionIndex": game.question_index
         }
-        for player in PLAYERS:
+        for player in game.players:
             await player.send(json.dumps(event))
 
-    async def relay_answers(self, websocket, game_code):
-        async for message in websocket:
-            host_ws = HOST[game_code]
+    async def relay_answers(self, player_socket, game):
+        async for message in player_socket:
+            host_ws = game.host_socket
             if host_ws.close_code is not None:
                 print("WebSocket connection is closed.")
                 break
             # TODO: Should validate here that the question index passed actually matches the server's index
             event = json.loads(message)
-            answer = Answer(game_code, event["teamName"], event["answer"], int(event["questionIndex"]))
-            self.db.insert_answer(answer)
+            answer = Answer(game.game_code, event["teamName"], event["answer"], int(event["questionIndex"]))
+            game.add_answer(answer)
             answer_received_event = {
                 "type": "answerReceived",
-                "questionIndex": self.question_index
+                "questionIndex": game.question_index
             }
-            await websocket.send(json.dumps(answer_received_event))
+            await player_socket.send(json.dumps(answer_received_event))
             await host_ws.send(message)
             
 
     async def create(self, websocket, game_code):
-        HOST[game_code] = websocket
-        self.game_code = game_code
+        game = Game(game_code, websocket)
+        self.games[game_code] = game
         print("Created a new game with game code", game_code)
         try:
             event = {
                 "type": "success",
                 "message": "Successfully created a new game",
                 "gameCode": game_code,
-                "questionIndex": self.question_index
+                "questionIndex": game.question_index
             }
             await websocket.send(json.dumps(event))
-            await self.host(websocket)
+            await self.host(websocket, self.games[game_code])
         finally:
             # del JOIN[game_code]
             pass
 
-    async def join(self, websocket, game_code):
-        if game_code not in HOST:
-            await self.send_error(websocket, "Invalid game code")
+    async def join(self, player_socket, game_code):
+        if game_code not in self.games:
+            await self.send_error(player_socket, "Invalid game code")
             print("invalid game code")
             return
+        game = self.games[game_code]
         print("Joined the game with game code", game_code)
-        PLAYERS.append(websocket)
+        game.players.append(player_socket)
         try:
             event = {
                 "type": "success",
                 "message": "Successfully joined the game",
                 "gameCode": game_code,
-                "questionIndex": self.question_index
+                "questionIndex": game.question_index
             }
-            await websocket.send(json.dumps(event))
-            await self.relay_answers(websocket, game_code)
+            await player_socket.send(json.dumps(event))
+            await self.relay_answers(player_socket, game)
         finally:
-            del HOST[game_code]
+            # del HOST[game_code]
+            # TODO: figure out what we need to delete
+            pass
 
 
     async def handler(self, websocket):
@@ -139,7 +127,7 @@ class Server:
 
     async def main(self):
         self.db = TriviaDatabase()
-        self.question_index = 1
+        self.games = {}
         loop = asyncio.get_running_loop()
         stop = loop.create_future()
         loop.add_signal_handler(signal.SIGTERM, stop.set_result, None)
