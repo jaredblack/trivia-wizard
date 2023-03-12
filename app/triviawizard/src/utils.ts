29d473076ebb8bbdfec15284dd77a84065789aa6
jaredblack <jrdblck@gmail.com>
2023-03-12 17:31:53 -0600

    Prep for Vercel deployment

--- app/triviawizard/src/utils.ts
+++ app/triviawizard/src/utils.ts
@@ -1,5 +1,5 @@
 export function getWebSocketServer() {
-    if (window.location.host ==="jaredblack.github.io") {
+    if (window.location.host ==="jaredblack.github.io" || window.location.host === "trivia-wizard.vercel.app") {
         return "wss://triviawizard.herokuapp.com";
     } else {
         return "ws://localhost:8001";
